9365197dbc4f77bd116f55d06e49bbf24d4cdf3e
JuninhoAlves <junior.alves.2007@gmail.com>
2020-01-25 23:28:17 -0300

    ajustes de interface

--- static/sketch.js
+++ static/sketch.js
@@ -1,22 +1,25 @@
 
 let dataset = {}
 let continuaTreino = true
-let limiteTeste = 10000
+let limiteTeste = 1000
 let redeNeural
 
 function setup() {
-    
-    redeNeural = new RedeNeural(2,3,1)
+    createCanvas(displayWidth, displayHeight);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+
+    redeNeural = new RedeNeural(2, 4, 1)
 
     //redeNeural.traine([1,2],[1])
 
     //apenas testa o xor
     dataset = {
         inputs: [
-            [0,0],
-            [1,1],
-            [1,0],
-            [0,1],
+            [0, 0],
+            [1, 1],
+            [1, 0],
+            [0, 1],
         ],
         output: [
             [0],
@@ -28,23 +31,44 @@ function setup() {
 }
 
 function draw() {
-    if(continuaTreino){
+    background(255)
+
+    textSize(30)
+    text("Rede Neural", 300, 100)
+    textSize(20)
+    text("- XOR -", 300, 130)
+    if (continuaTreino) {
         let numTeste = 0;
-        while(numTeste < limiteTeste){
+        while (numTeste < limiteTeste) {
             let index = numTeste % dataset.inputs.length
             redeNeural.traine(dataset.inputs[index], dataset.output[index])
             numTeste++
         }
-        let testeUm = redeNeural.predict([1,1])[0] > 0.991
-        let testeDois = redeNeural.predict([1,0])[0] < 0.004
-        if(testeUm && testeDois){
+        let testeUm = redeNeural.predict([1, 0])[0] > 0.98
+        let testeDois = redeNeural.predict([1, 1])[0] < 0.02
+        if (testeUm && testeDois) {
             continuaTreino = false
-            console.log("rede treinada com sucesso")
-        }else{
-            console.clear()
-            console.log(" 1 0 = "  + redeNeural.predict([0,1])[0])
-            console.log(" 1 1 = " + redeNeural.predict([1,1])[0])
-            console.log("ainda treinando")
+            console.log("Para testar a rede utilize")
+            console.log("Tabela Xor")
+            console.table([
+                ["entrada", "entrada", "resultado"],
+                [0,0,0],
+                [0,1,1],
+                [1,0,1],
+                [1,1,0]
+            ])
+            console.log("redeNeural.predict([0,1])[0]")
+            console.log("redeNeural.predict([1,1])[0]")
+        } else {
+            textSize(15)
+            text("1 XOR 0 = " + redeNeural.predict([0, 1])[0].toFixed(4), 300, 160)
+            text("1 XOR 1 = " + redeNeural.predict([1, 1])[0].toFixed(4), 300, 180)
+            text("(Treinando)", 300, 200)
+
         }
+    } else {
+        textSize(15)
+        text("Rede treinada com sucesso, aperte F12", 300, 160)
+        text("e confira no console.", 300, 180)
     }
 }
